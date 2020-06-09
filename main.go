6d69a965e8245fec62a3d77ce241143c073bb9b0
ivan-fr <ivan.besevic_fr@yahoo.com>
2020-06-09 22:06:32 +0200

    Improve code

--- main.go
+++ main.go
@@ -21,6 +21,21 @@ func main() {
 		OrderBy(map[string]interface{}{"bibi": "", "lolo": "DESC", "palopalo": "DESC"}).
 		GroupBy([]string{"IdUser", "Koko", "Pipi"})
 	sQueryBuilder.RollUp = true
+
+	fmt.Println(sQueryBuilder.ConstructSql())
+	fmt.Println(sQueryBuilder.GetStmts())
+
+	sQueryBuilder.Clean()
+
+	sQueryBuilder = sQueryBuilder.
+		FindBy(map[string]interface{}{"id": "lol", "koko": 9, "giro": nil}).
+		Aggregate(map[string]interface{}{"COUNT": "*", "AVG": "IdUser"}).
+		Having(map[string]interface{}{"COUNT__lte": []interface{}{"*", 10}, "AVG__gt": []interface{}{"IdUser", 13}}).
+		Consider("InitiatedThread").
+		Consider("ReceivedThread").
+		Consider("Roles").
+		OrderBy(map[string]interface{}{"bibi": "", "lolo": "DESC", "palopalo": "DESC"}).
+		GroupBy([]string{"IdUser", "Koko", "Pipi"})
 	fmt.Println(sQueryBuilder.ConstructSql())
 	fmt.Println(sQueryBuilder.GetStmts())
 }
