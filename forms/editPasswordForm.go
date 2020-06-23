5b81180377dd45e329e4292e8e94e00087997d4f
ivan-fr <ivan.besevic_fr@yahoo.com>
2020-06-23 22:04:41 +0200

    Improve code

--- forms/editPasswordForm.go
+++ forms/editPasswordForm.go
@@ -2,5 +2,5 @@ package forms
 
 type EditPasswordForm struct {
 	EncryptedPassword string `form:"password" validate:"required,min=5"`
-	ConfirmPassword string `form:"confirmPassword" validate:"required,min=5"`
+	ConfirmPassword   string `form:"confirmPassword" validate:"required,min=5"`
 }
